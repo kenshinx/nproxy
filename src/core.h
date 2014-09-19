9e6a5ae6d285a217a625aa1a09705fbbc58dacb6
kenshin <kenshin.acs@gmail.com>
2014-09-19 11:49:18 +0800

    core,h

---
+++ src/core.h
@@ -0,0 +1,21 @@
+
+#ifndef _NPROXY_CORE_H_
+#define _NPROXY_CORE_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
+#include "string.h"
+#include "array.h"
+#include "util.h"
+#include "log.h"
+
+#define CRLF                        "\r\n"
+#define NP_OK                       0
+#define NP_ERROR                    -1
+
+typedef int np_status_t;
+
+#endif
