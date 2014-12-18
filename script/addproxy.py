af6c7ada356ca008220c9b53189edad362f58c2c
kenshin <kenshin.acs@gmail.com>
2014-12-18 17:11:09 +0800

    to enhance import script

--- script/addproxy.py
+++ script/addproxy.py
@@ -3,6 +3,7 @@
 import sys
 import redis
 import json
+import optparse
 
 REDIS_HOST = "dev1.netlab.corp.qihoo.net"
 REDIS_PORT = 6379
@@ -18,14 +19,24 @@ def add(host, port, proto="socks5", username=None, password=None):
                 "username": username,
                 "password": password})
 
+    print data
     r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
     r.sadd(REDIS_KEY, data)
 
     print "sucess"
     
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-u", "--username", action="store", dest="username", type="string")
+    parser.add_option("-p", "--password", action="store", dest="password", type="string")
+    parser.add_option("", "--proto", action="store", dest="proto", type="string", default="socks5")
+    options, args = parser.parse_args()
+
+    host, port = args[0].split(":")
+    
+    add(host, port, options.proto, options.username, options.password)
     
 
 if __name__ == "__main__":
-    host = sys.argv[1]
-    port = sys.argv[2]
-    add(host, port)
+    main()
